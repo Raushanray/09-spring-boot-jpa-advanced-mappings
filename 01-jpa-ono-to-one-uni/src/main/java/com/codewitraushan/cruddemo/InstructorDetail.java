348f4c655a2dba626fb76c516658b05065bc4768
Raushanray <raushankuray1998@gmail.com>
2023-07-08 20:08:56 +0530

    Add a command line runner in main class and configure the db and also create a InstructorDetail in entity package..

---
+++ 01-jpa-ono-to-one-uni/src/main/java/com/codewitraushan/cruddemo/InstructorDetail.java
@@ -0,0 +1,71 @@
+package com.codewitraushan.cruddemo;
+
+import jakarta.persistence.*;
+
+//annotate the class as an entity map to db table
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+
+
+
+    //Define the fields
+
+    //annotate the fields with db column name
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+    //create constructor
+    public InstructorDetail(){
+
+    }
+
+    public InstructorDetail(String youtubeChannel, String hobby) {
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+    //genetate the getter/setters method
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+
+    //generate the toString method
+
+    @Override
+    public String toString() {
+        return "InstructorDetail{" +
+                "id=" + id +
+                ", youtubeChannel='" + youtubeChannel + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}
