033fc3847ac43323cc7f402591c934acd7d98063
Raushanray <raushankuray1998@gmail.com>
2023-07-10 10:59:54 +0530

    create a spring boot command line app and also create a new database schemas hb-03-one-to-many here created three table instructor,instructor details and course, relationship b/w tables instructor and instructor details and connect with one to one mapping and instructor and course are connected with one to many relationship.. now we want to create a instructor...

---
+++ 03-jpa-ono-to-many/src/main/java/com/codewitraushan/cruddemo/CruddemoApplication.java
@@ -0,0 +1,142 @@
+package com.codewitraushan.cruddemo;
+
+import com.codewitraushan.cruddemo.dao.AppDAO;
+import com.codewitraushan.cruddemo.entity.Course;
+import com.codewitraushan.cruddemo.entity.Instructor;
+import com.codewitraushan.cruddemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO){
+		return runner-> {
+			//createInstructor(appDAO);
+//			findInstructor(appDAO);
+//			deleteInstructor(appDAO);
+//			findInstructorDetail(appDAO);
+
+//			deleteInstructorDetail(appDAO);
+
+			createInstructorWithCourses(appDAO);
+		};
+
+	}
+
+	private void createInstructorWithCourses(AppDAO appDAO) {
+		//create the Instructor
+		Instructor tempInstructor = new
+				Instructor("Tannu","raj","tannu@gmail.com");
+
+		//create the Instructordetails
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail("http://www.youtube.com","video_game!!");
+
+
+		//associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		//create some course
+		Course tempCourse1 = new Course("Air Guiter - the ultimate guide");
+		Course tempCourse2 = new Course("the pinball masterclass");
+
+		//add course to instructor
+		tempInstructor.add(tempCourse1);
+		tempInstructor.add(tempCourse2);
+
+		//save the instructor
+		//
+		//Note: this will also save the courses
+		//because of CascadeType.PERSIST
+		//
+		System.out.println("Saving instructor : " + tempInstructor);
+		System.out.println("the courses : "+ tempInstructor.getCourses());
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!!");
+	}
+
+	private void deleteInstructorDetail(AppDAO appDAO) {
+		int theId = 3;
+		System.out.println("Deleting the instructor details : " + theId);
+
+		appDAO.deleteInstructorDetailById(theId);
+		System.out.println("Done!!");
+	}
+
+	private void findInstructorDetail(AppDAO appDAO) {
+		//get the innstructor details object
+		int theId = 2;
+		InstructorDetail tempInstructorDetail = appDAO.findInstructorDetailById(theId);
+
+		//print the instructor details
+		System.out.println("tempInstructorDetail : " + tempInstructorDetail);
+
+		//print the associated instructor
+		System.out.println("the associated instructor : " + tempInstructorDetail.getInstructor());
+
+		System.out.println("Done!!");
+
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+		int theId = 1;
+		System.out.println("Deleting instructor id :- " + theId);
+		appDAO.deleteInstructorById(theId);
+		System.out.println("Done!!");
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+		int theId = 2;
+		System.out.println("finding Instructor id :- " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+		System.out.println("tempInstructor" + tempInstructor);
+		System.out.println("the associated instructureDetails only " + tempInstructor.getInstructorDetail());
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+		/*
+		//create the Instructor
+		Instructor tempInstructor = new
+				Instructor("Ravi","ranjan","ravi@gmail.com");
+
+		//create the Instructordetails
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail("http://www.bihar_vibes.com/youtube","love to code!!");
+
+
+		 */
+
+		//create the Instructor
+		Instructor tempInstructor = new
+				Instructor("sumit","kumar","sumit@gmail.com");
+
+		//create the Instructordetails
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail("http://www.bihar_vibes.com/youtube","photography");
+
+
+		//associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		//save the instructor
+		//
+		//Note:- this will also save the details object
+		//because of CascadeType.ALL
+
+		System.out.println("Saving Instructor" + tempInstructor);
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!!");
+	}
+
+}
---
+++ 03-jpa-ono-to-many/src/main/java/com/codewitraushan/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,62 @@
+package com.codewitraushan.cruddemo.dao;
+
+import com.codewitraushan.cruddemo.entity.Instructor;
+import com.codewitraushan.cruddemo.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class AppDAOImpl implements AppDAO{
+
+    //define fields for entity manager
+    private EntityManager entityManager;
+
+    //inject entity manager using constructor injection
+    public AppDAOImpl(EntityManager entityManager){
+        this.entityManager=entityManager;
+    }
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+
+    }
+
+    @Override
+    public Instructor findInstructorById(int theid) {
+      return   entityManager.find(Instructor.class,theid);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int theid) {
+        // retrieve the instructor
+        Instructor tempInstructor = entityManager.find(Instructor.class,theid);
+        //delete the instructor
+        entityManager.remove(tempInstructor);
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int theId) {
+        return entityManager.find(InstructorDetail.class ,theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int theId) {
+        //retrieve the instructor details
+        InstructorDetail tempInstructorDetail = entityManager.find(InstructorDetail.class, theId);
+
+        //remove the associated object referene
+        //break bi-directional link
+        tempInstructorDetail.getInstructor().setInstructorDetail(null);
+
+
+        //delete the instructor details
+        entityManager.remove(tempInstructorDetail);
+    }
+
+
+
+}
